88abd9dd6b4126e4276a04cc11a02236a3a62fbc
amy <amykim4229@gmail.com>
2022-04-08 23:33:54 +0900

    0408 pratice3. seperate code by function

---
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -0,0 +1,42 @@
+package screen
+
+class ShoppingCategory {
+    fun selectCategory(){
+        println(
+            """  
+                원하시는 카테고리를 입력해주세요
+            *****=================================*****
+            """.trimMargin()
+        )
+        val categories = arrayOf("패션", "전자기기", "식음료")
+        for (category in categories) {
+            println(category)
+        }
+
+        println("장바구니로 돌아가시려면 #을 눌러주세요 ")
+
+        var selectedCategory = readLine()
+        while (selectedCategory.isNullOrBlank() ) {
+            println("출력된 카테고리 중 원하시는 카테고리를 입력해주세요")
+            selectedCategory = readLine()
+        }
+        if(selectedCategory == "#") {
+            // TODO 장바구니 이동
+        }else {
+
+            if (!categories.contains(selectedCategory)) {
+                showErrorMessage(selectedCategory)
+                selectCategory()
+            }else{
+                // TODO 카테고리 상품 출력
+            }
+
+        }
+    }
+
+        private fun showErrorMessage(selectedCategory: String?) {
+            println("[$selectedCategory] 존재하지 않는 카테고리 입니다. 다시 입력해주세요")
+
+        }
+
+}

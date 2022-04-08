ac6c8b8903be62e5cc28cb1c59a9045ee9fbb24b
amy <amykim4229@gmail.com>
2022-04-08 23:32:27 +0900

    0408 pratice3. seperate code by function

--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -4,18 +4,14 @@ class ShoppingHome {
 
     fun start(){
 
-        val customerName = greeting()
+        greeting()
 
-        val selectedCategory = selectCategory()
+        selectCategory()
 
-        if(selectedCategory == "#") {
-            // TODO 장바구니 이동
-        }else {
-            // TODO 카테고리 상품 출력
-        }
+
     }
 
-    private fun greeting() :String {
+    private fun greeting() {
         println("안녕하세요 에이미스토어 입니다.")
         var name =""
         while(name.isBlank()){
@@ -29,29 +25,11 @@ class ShoppingHome {
         
         """.trimMargin()
         )
-        return name
     }
 
-    private fun selectCategory(): String {
-        println(
-            """  
-                원하시는 카테고리를 입력해주세요
-            *****=================================*****
-            """.trimMargin()
-        )
-        val categories = arrayOf("패션", "전자기기", "식음료")
-        for (category in categories) {
-            println(category)
-        }
-
-        println("장바구니로 돌아가시려면 #을 눌러주세요 ")
-
-        var selectedCategory = readLine()
-        while (selectedCategory.isNullOrBlank() || (selectedCategory != "#" && !categories.contains(selectedCategory))) {
-            println("출력된 카테고리 중 원하시는 카테고리를 입력해주세요")
-            selectedCategory = readLine()
-        }
-        return selectedCategory
+    private fun selectCategory() {
+        val shoppingCategory = ShoppingCategory()
+        shoppingCategory.selectCategory()
     }
 
 
